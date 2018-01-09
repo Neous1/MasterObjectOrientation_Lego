30825e2980a5dcfd70151379b48ee93c61be1ea9
Neous1 <Neous1@users.noreply.github.com>
2018-01-09 14:30:38 -0500

    create a class factoryWorkder create a method AddNumbers where result is returned

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new FactoryWorker();
+            var result = worker.AddNumbers(2, 3);
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+
+    class FactoryWorker
+    {
+        public FactoryWorker()
+        {
+            
+        }
+
+        //public TYPE Type { get; set; }
+
+        public int AddNumbers(int num1, int num2)
+        {
+            var result = num1 + num2;
+            return result;
+        }
+    }
+}
