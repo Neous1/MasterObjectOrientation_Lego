c542dc92e48052c66dbb776fb2693ad58e717059
Neous1 <Neous1@users.noreply.github.com>
2018-01-08 20:07:49 -0500

    Create bulldozer class create dozer object

---
+++ Bulldozer/Bulldozer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulldozer
+{
+    class BullDozerClass
+    {
+        public string color { get; set; }
+        public string size { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dozer = new BullDozerClass();
+            dozer.color = "Yellow";
+            dozer.size = "Mini";
+
+            Console.WriteLine("Bulldozer is: " + dozer.color);
+        }
+    }
+}
