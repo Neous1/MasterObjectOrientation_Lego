9f16234e6f921cf866e92124c6260013fa38d54a
Neous1 <Neous1@users.noreply.github.com>
2018-01-10 23:45:15 -0500

    create a SaloonCar class with 1 prop and 3 constructor

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -12,6 +12,25 @@ namespace Homework
         public string Manufacturer { get; set; }
 
     }
+
+    class SaloonCar : Car
+    {
+        public int NumberOfSeats { get; set; }
+
+        public SaloonCar(int numberOfSeats)
+        {
+            
+        }
+
+        public SaloonCar(int numberOfSeats, string manufacturer)
+        {
+            
+        }
+        public SaloonCar(int numberOfSeats, string manufacturer, string colour)
+        {
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
