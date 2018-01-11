f9fba8cd2cc7c06834447f9440007138c42bbef1
Neous1 <Neous1@users.noreply.github.com>
2018-01-10 23:41:28 -0500

    Create a car class prop color and manufacturer

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Car
+    {
+        public string Colour { get; set; }
+        public string Manufacturer { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
